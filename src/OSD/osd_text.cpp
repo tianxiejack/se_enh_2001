d67f876b4b851dba6b151db1d4abb18ab724fd91
alex <trackxt@qq.com>
2018-07-30 15:05:34 +0800

    增加了字体切换，由于黑体在标清下太粗，目前黑体实际用的是 方正兰亭超细黑简体 常规

--- src/OSD/osd_text.cpp
+++ src/OSD/osd_text.cpp
@@ -27,37 +27,57 @@ void OSDCreatText()
 	
 	_font_hd_small_st   =   new FreeTypeFont();
 	_font_hd_small_st->create("simsun.ttc",25,512,512);
-
+	
 	_font_pal_big_st   =   new FreeTypeFont();
 	_font_pal_big_st->create("simsun.ttc",16,512,512);
 	
 	_font_pal_small_st   =   new FreeTypeFont();
 	_font_pal_small_st->create("simsun.ttc",12,512,512);
+
+	_font_hd_big_ht   =   new FreeTypeFont();
+	_font_hd_big_ht->create("FZLTCXHJW.TTF",46,512,512);
 	
+	_font_hd_small_ht   =   new FreeTypeFont();
+	_font_hd_small_ht->create("FZLTCXHJW.TTF",32,512,512);
+	
+	_font_pal_big_ht   =   new FreeTypeFont();
+	_font_pal_big_ht->create("FZLTCXHJW.TTF",22,512,512);
+	
+	_font_pal_small_ht   =   new FreeTypeFont();
+	_font_pal_small_ht->create("FZLTCXHJW.TTF",18,512,512);
+
 }
 
 void OSDdrawText(int x,int y,wchar_t* text,char font,char fontsize,int win_width,int win_height)
 {
+	FreeTypeFont* pTmp  = NULL;
 	if(win_width == 1920){
-		if(fontsize == 0x05){
-			_font_hd_big_st->begin(win_width,win_height);
-			_font_hd_big_st->drawText(x,y,0,Rgba(255,255,255,255),text,0,0,0);
-			_font_hd_big_st->end();
+		if(font == 0x02){
+			if(fontsize == 0x06)
+				pTmp = _font_hd_small_ht;
+			else
+				pTmp = _font_hd_big_ht;
 		}else{
-			_font_hd_small_st->begin(win_width,win_height);
-			_font_hd_small_st->drawText(x,y,0,Rgba(255,255,255,255),text,0,0,0);
-			_font_hd_small_st->end();
+			if(fontsize == 0x06)
+				pTmp = _font_hd_small_st;
+			else
+				pTmp = _font_hd_big_st;
 		}
 	}else{
-		if(fontsize == 0x05){
-			_font_pal_big_st->begin(win_width,win_height);
-			_font_pal_big_st->drawText(x,y,0,Rgba(255,255,255,255),text,0,0,0);
-			_font_pal_big_st->end();
+		if(font == 0x02){
+			if(fontsize == 0x06)
+				pTmp = _font_pal_small_ht;
+			else
+				pTmp = _font_pal_big_ht;
 		}else{
-			_font_pal_small_st->begin(win_width,win_height);
-			_font_pal_small_st->drawText(x,y,0,Rgba(255,255,255,255),text,0,0,0);
-			_font_pal_small_st->end();
+			if(fontsize == 0x05)
+				pTmp = _font_pal_small_st;
+			else
+				pTmp = _font_pal_big_st;
 		}
 	}
+	pTmp->begin(win_width,win_height);
+	pTmp->drawText(x,y,0,Rgba(255,255,255,255),text,0,0,0);
+	pTmp->end();
 	return ;
 }
