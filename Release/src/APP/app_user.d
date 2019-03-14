9f4beff5ec04b47f3532340ab595a0f6623ee85f
alex <alex@qr.com>
2019-03-14 19:26:54 +0800

    更新makefile

--- Release/src/APP/app_user.d
+++
@@ -1,2 +0,0 @@
-src/APP/app_user.o : ../src/APP/app_user.cpp \
-    /usr/include/stdc-predef.h
--- Release/src/DXUTC/sys_config.d
+++
@@ -1,2 +0,0 @@
-src/DXUTC/sys_config.o : ../src/DXUTC/sys_config.cpp \
-    /usr/include/stdc-predef.h
